1e7c078f24ee41f1ef6c9cdec0ae1b81b88ed9b0
Rifky <muhamad210501@gmail.com>
2025-08-02 15:06:29 +0700

    Update ad display logic in public layout: Simplify condition for banner320x50 to check for existence instead of image URL, ensuring proper rendering of ads.

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -123,7 +123,7 @@
 							</p>
 						</div>
 
-                        @if($banner320x50[0] && !empty($banner320x50[0]->image_url))
+                        @if($banner320x50[0])
                             <div >
                                 <a href="{{ $banner320x50[0]->link_url }}" target="_blank" rel="noopener noreferrer">
                                     <img src="{{ asset('storage/' . $banner320x50[0]->image_url) }}" alt="{{ $banner320x50[0]->title }}" class="banner-ad">
